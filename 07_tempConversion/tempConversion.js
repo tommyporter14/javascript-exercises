0f1d88a8e74a65c693f33693430c857ef6a4b7e6
tommy <100875138+tommyporter14@users.noreply.github.com>
2024-12-16 14:34:28 -0500

    7 finished - temp conversion

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,11 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(tempf) {
+  let tempc = (tempf - 32) * 0.55555;
+  return Math.round(tempc * 10) / 10;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(tempc) {
+  let tempf = tempc * 1.8 +32;
+  return Math.round(tempf * 10) / 10;
 };
 
 // Do not edit below this line
