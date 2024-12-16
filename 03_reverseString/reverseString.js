f311710698e772c135e2a1837eded6383aa3a291
tommy <100875138+tommyporter14@users.noreply.github.com>
2024-12-16 12:28:22 -0500

    3 finishes - reverse string

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,9 @@
-const reverseString = function() {
-
+const reverseString = function(word) {
+    const myArray = [];
+    for ( i = word.length; i >= 0; i--) {
+        myArray.push(word.charAt(i));
+    }
+    return myArray.join("");
 };
 
 // Do not edit below this line
