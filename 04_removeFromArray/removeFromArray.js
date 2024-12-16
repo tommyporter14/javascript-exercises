4c388ff2d807d932abb10c5ea7db764536bd4c99
tommy <100875138+tommyporter14@users.noreply.github.com>
2024-12-16 13:19:13 -0500

    4 finished - remove from array

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,7 @@
-const removeFromArray = function() {
-};
+const removeFromArray = function(collection, ...remove) {
+      const newArray = collection.filter(item => !remove.includes(item));
+      return newArray;
+}
 
 // Do not edit below this line
 module.exports = removeFromArray;
