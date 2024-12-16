acc0e780c18b9e2a8a1d57ad1ee0b6244c3ea30a
tommy <100875138+tommyporter14@users.noreply.github.com>
2024-12-16 14:07:45 -0500

    5 finished - sum all

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,22 @@
-const sumAll = function() {
-
+const sumAll = function(num1, num2) {
+    let first;
+    let second;
+    let result = 0;
+    if (num1 < 0 || num2 < 0 || !(typeof num1 === 'number') || !(typeof num2 === 'number')
+        || !(Number.isInteger(num1)) || !(Number.isInteger(num2))){
+        result = "ERROR";
+    }
+    else if(num1 < num2){
+        first = num1;
+        second = num2;
+    } else {
+        first = num2;
+        second = num1;
+    }
+    for (let i = first; i <= second; i++){
+        result = result + i;
+    }
+    return result;
 };
 
 // Do not edit below this line
