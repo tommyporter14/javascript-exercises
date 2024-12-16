2dc113b553caf4df5bd675c6843737f4b2d54813
tommy <100875138+tommyporter14@users.noreply.github.com>
2024-12-16 12:14:52 -0500

    2 finished - repeating string

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,12 @@
-const repeatString = function() {
-
+const repeatString = function(word, num) {
+    let result = "";
+    if (num < 0){
+        result ="ERROR";
+    }
+    for(i = 0; i < num; i++ ){
+            result = result + word;
+    }
+    return result;
 };
 
 // Do not edit below this line
